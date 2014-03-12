00c545a048f7a9b84ce2eee00bbb7dc903c4fbcb
nacho <ignacioperez11@gmail.com>
2014-03-12 12:26:29 +0100

    Added sample mark and route

--- app/src/main/java/lbs/erasmus/touristanbul/fragments/MapFragment.java
+++ app/src/main/java/lbs/erasmus/touristanbul/fragments/MapFragment.java
@@ -5,17 +5,27 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import org.osmdroid.ResourceProxy;
+import org.osmdroid.bonuspack.overlays.Marker;
+import org.osmdroid.bonuspack.overlays.Polyline;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.tileprovider.MapTileProviderBasic;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.TilesOverlay;
+
+import java.util.ArrayList;
 
 import lbs.erasmus.touristanbul.R;
 
@@ -31,15 +41,17 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
+        // Adding the map
         MapView mapView = (MapView) rootView.findViewById(R.id.mapView);
 
         mapView.setClickable(true);
         mapView.setMultiTouchControls(true);
-        mapView.getController().setZoom(14);
+        mapView.getController().setZoom(11);
         mapView.setMaxZoomLevel(18);
-        mapView.setMinZoomLevel(14);
+        mapView.setMinZoomLevel(11);
         // Location: Istanbul
         mapView.getController().setCenter(new GeoPoint(41.0096334, 28.9651646));
+        //mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
 
         // Add tiles layer with custom tile source
         final MapTileProviderBasic tileProvider = new MapTileProviderBasic(getActivity().getApplicationContext());
@@ -52,6 +64,31 @@ public class MapFragment extends Fragment {
         tilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
         mapView.getOverlays().add(tilesOverlay);
 
+        // Adding a mark
+        Marker startMarker = new Marker(mapView);
+        startMarker.setPosition(new GeoPoint(40.97244, 29.15236));
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setIcon(getResources().getDrawable(android.R.drawable.ic_menu_compass));
+        startMarker.setTitle("Yeditepe University");
+        startMarker.setDraggable(true);
+        mapView.getOverlays().add(startMarker);
+
+        // Adding a route
+        RoadManager roadManager = new OSRMRoadManager();
+        //RoadManager roadManager = new MapQuestRoadManager("YOUR_API_KEY");
+        //roadManager.addRequestOption("routeType=bicycle");
+        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+        waypoints.add(new GeoPoint(40.98196, 28.82070));
+        waypoints.add(new GeoPoint(40.97244, 29.15236)); //end point
+        Road road = roadManager.getRoad(waypoints);
+        // Build a Polyline with the route shape
+        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, getActivity().getApplicationContext());
+        // Add this Polyline to the overlays
+        mapView.getOverlays().add(roadOverlay);
+
+        // Refresh map
+        mapView.invalidate();
+
         return rootView;
     }
 
@@ -63,6 +100,17 @@ public class MapFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_example) {
+            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }

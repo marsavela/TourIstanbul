49e61ac2f487bab1f88df2ae40faff5b2bce0b6f
igpremuo <igpremuo@inf.upv.es>
2014-03-25 00:59:33 +0100

    Changed maps library to mapsforge 0.4.0

--- app/src/main/java/lbs/erasmus/touristanbul/fragments/MapFragment.java
+++ app/src/main/java/lbs/erasmus/touristanbul/fragments/MapFragment.java
@@ -1,98 +1,69 @@
 package lbs.erasmus.touristanbul.fragments;
 
 import android.app.Fragment;
-import android.graphics.Color;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
-import org.osmdroid.ResourceProxy;
-import org.osmdroid.bonuspack.overlays.Marker;
-import org.osmdroid.bonuspack.overlays.Polyline;
-import org.osmdroid.bonuspack.routing.OSRMRoadManager;
-import org.osmdroid.bonuspack.routing.Road;
-import org.osmdroid.bonuspack.routing.RoadManager;
-import org.osmdroid.tileprovider.MapTileProviderBasic;
-import org.osmdroid.tileprovider.tilesource.ITileSource;
-import org.osmdroid.tileprovider.tilesource.XYTileSource;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.TilesOverlay;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.mapsforge.core.graphics.Bitmap;
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.core.model.MapPosition;
+import org.mapsforge.map.android.AndroidPreferences;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.layer.MyLocationOverlay;
 
-import java.util.ArrayList;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.layer.LayerManager;
+import org.mapsforge.map.layer.Layers;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.download.TileDownloadLayer;
+import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
+import org.mapsforge.map.model.MapViewPosition;
+import org.mapsforge.map.model.common.PreferencesFacade;
+
+import java.io.File;
 
 import lbs.erasmus.touristanbul.R;
+import lbs.erasmus.touristanbul.maps.Utils;
 
 /**
  * Created by SergiuDaniel on 9/03/14.
  */
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-    }
+    private MapView mMapView;
+    protected PreferencesFacade mPreferencesFacade;
+    protected TileCache mTileCache;
 
-    MyLocationNewOverlay myLocationOverlay = null;
+    private MyLocationOverlay mMyLocationOverlay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-
         // Adding the map
-        MapView mapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
 
-        mapView.setClickable(true);
-        mapView.setMultiTouchControls(true);
-        mapView.getController().setZoom(11);
-        mapView.setMaxZoomLevel(18);
-        mapView.setMinZoomLevel(11);
-        // Location: Istanbul
-        mapView.getController().setCenter(new GeoPoint(41.0096334, 28.9651646));
-        //mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
+        // Load last map position
+        //SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getPersistableId(), Context.MODE_PRIVATE);
+        //mPreferencesFacade = new AndroidPreferences(sharedPreferences);
 
-        // Add tiles layer with custom tile source
-        final MapTileProviderBasic tileProvider = new MapTileProviderBasic(getActivity().getApplicationContext());
-        // Mapnik map tiles
-        final ITileSource tileSource = new XYTileSource("Mapnik", ResourceProxy.string.mapnik, 1, 18, 256, ".png", new String[] { "http://tile.openstreetmap.org/"});
-        // MapQuest map tiles
-        //final ITileSource tileSource = new XYTileSource("MapQuest", ResourceProxy.string.mapquest_osm, 1, 21, 256, ".png", new String[] {"http://otile1.mqcdn.com/tiles/1.0.0/osm/","http://otile2.mqcdn.com/tiles/1.0.0/osm/"});
-        tileProvider.setTileSource(tileSource);
-        final TilesOverlay tilesOverlay = new TilesOverlay(tileProvider, getActivity().getBaseContext());
-        tilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
-        mapView.getOverlays().add(tilesOverlay);
+        initialize();
 
-        // Adding a mark
-        Marker startMarker = new Marker(mapView);
-        startMarker.setPosition(new GeoPoint(40.97244, 29.15236));
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setIcon(getResources().getDrawable(android.R.drawable.ic_menu_compass));
-        startMarker.setTitle("Yeditepe University");
-        startMarker.setDraggable(true);
-        mapView.getOverlays().add(startMarker);
-
-        // Adding a route
-        RoadManager roadManager = new OSRMRoadManager();
-        //RoadManager roadManager = new MapQuestRoadManager("YOUR_API_KEY");
-        //roadManager.addRequestOption("routeType=bicycle");
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-        waypoints.add(new GeoPoint(40.98196, 28.82070));
-        waypoints.add(new GeoPoint(40.97244, 29.15236)); //end point
-        Road road = roadManager.getRoad(waypoints);
-        // Build a Polyline with the route shape
-        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, getActivity().getApplicationContext());
-        // Add this Polyline to the overlays
-        mapView.getOverlays().add(roadOverlay);
-
-        // Show users location
-        myLocationOverlay = new MyLocationNewOverlay(getActivity().getApplicationContext(), mapView);
-        mapView.getOverlays().add(myLocationOverlay);
-        myLocationOverlay.enableMyLocation();
-
-        // Refresh map
-        mapView.invalidate();
+        Button button = (Button) rootView.findViewById(R.id.mapfragment_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //shows user current location
+                showCurrentPosition();
+            }
+        });
 
         return rootView;
     }
@@ -108,14 +79,123 @@ public class MapFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        // Save current map position
+        //mMapView.getModel().save(mPreferencesFacade);
+        //mPreferencesFacade.save();
+    }
+
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void addLayers(LayerManager layerManager, TileCache tileCache, MapViewPosition mapViewPosition) {
+        layerManager.getLayers().add(Utils.createTileRendererLayer(tileCache, mapViewPosition, getMapFile()));
+    }
+
+    protected TileCache createTileCache() {
+        return Utils.createExternalStorageTileCache(getActivity(), getPersistableId());
+    }
+
+    /**
+     * @return center position of Istanbul (41.0096334, 28.9651646)
+     */
+    protected MapPosition getInitialPosition() {
+        return new MapPosition(new LatLong(41.0096334, 28.9651646), (byte) 12);
+    }
+
+    /**
+     * @return a map file
+     */
+    protected File getMapFile() {
+        return new File(Environment.getExternalStorageDirectory(), getMapFileName());
+    }
+
+    /**
+     * @return the map file name to be used
+     */
+    protected String getMapFileName() {
+        //TODO: download istanbul map to a SD folder
+        return "valenciamaps/spain.map";
+    }
+
+    /**
+     * @return the id that is used to save this mapview
+     */
+    protected String getPersistableId() {
+        return this.getClass().getSimpleName();
+    }
+
+    /**
+     * Initializes the map view
+     */
+    protected void initialize() {
+        initializeMapView(mMapView, mPreferencesFacade);
+        mTileCache = createTileCache();
+        MapViewPosition mapViewPosition = initializePosition(mMapView.getModel().mapViewPosition);
+
+        //TODO: Change online maps for offline maps, add a download map dialog
+        //addLayers(mMapView.getLayerManager(), mTileCache, mapViewPosition); // Offline map
+
+        TileDownloadLayer downloadLayer = new TileDownloadLayer(mTileCache, mapViewPosition, OpenStreetMapMapnik.INSTANCE,
+                AndroidGraphicFactory.INSTANCE);
+        downloadLayer.start();
+        mMapView.getLayerManager().getLayers().add(downloadLayer);
+
+    }
+
+    /**
+     * Initializes the map view
+     *
+     * @param mapView
+     */
+    protected void initializeMapView(MapView mapView, PreferencesFacade preferences) {
+        //mapView.getModel().init(preferences);
+        mapView.setClickable(true);
+        mapView.getMapScaleBar().setVisible(true);
+    }
+
+    /**
+     * Initializes the map view position
+     *
+     * @param mapViewPosition
+     *            the map view position to be set
+     * @return the mapviewposition set
+     */
+    protected MapViewPosition initializePosition(MapViewPosition mapViewPosition) {
+        LatLong center = mapViewPosition.getCenter();
+        if (center.equals(new LatLong(0, 0))) {
+            mapViewPosition.setMapPosition(getInitialPosition());
+        }
+        return mapViewPosition;
+    }
+
+    /**
+     * Shows users location in the map
+     */
+    private void showCurrentPosition() {
+        // a marker to show at the position
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_drawer);
+        Bitmap bitmap = AndroidGraphicFactory.convertToBitmap(drawable);
+        MapViewPosition mapViewPosition = mMapView.getModel().mapViewPosition;
+        LayerManager layerManager = mMapView.getLayerManager();
+
+        // create the overlay and tell it to follow the location
+        mMyLocationOverlay = new MyLocationOverlay(getActivity(), mapViewPosition, bitmap);
+        mMyLocationOverlay.setSnapToLocationEnabled(true);
+
+        layerManager.getLayers().add(mMyLocationOverlay);
+        mMyLocationOverlay.enableMyLocation(true);
+
     }
 
 }

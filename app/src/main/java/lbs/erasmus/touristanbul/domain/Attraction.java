4dbbe54daa5a9ce93bc8a798ec38702a5cf386c9
Lorena <lorenacalabuig@gmail.com>
2014-04-06 01:46:17 +0200

    Modified Attraction class      - New constructor      - New variables

--- app/src/main/java/lbs/erasmus/touristanbul/domain/Attraction.java
+++ app/src/main/java/lbs/erasmus/touristanbul/domain/Attraction.java
@@ -24,7 +24,28 @@ public class Attraction implements Parcelable {
     private final String mSubtitle;
 
     /** Description of the attraction. */
-    //private final Location mLocation;
+    private String mDescription;
+
+    /** Address of the attraction. */
+    private String mAddress;
+
+    /** Description of the attraction. */
+    private Location mLocation;
+
+    /** Category of the attraction. */
+    private String mCategory;
+
+    /** Interest of the attraction. */
+    private String mInterest;
+
+    /** Rate of the attraction. */
+    private double mRate;
+
+    /** Number of rates of the attraction. */
+    private int mNumRates;
+
+    /** Opening times of the attraction. */
+    private String mOpeningTimes;
 
     /** Content URI of the image for the attraction. */
     private final Uri mImageUri;
@@ -40,7 +61,42 @@ public class Attraction implements Parcelable {
     public Attraction(String titleString, String subtitleString, Location location, String imageAssetFilePath) {
         mTitle = titleString;
         mSubtitle = subtitleString;
-        //mLocation = location;
+        mLocation = location;
+        mImageUri = Uri.parse("content://" + AssetProvider.CONTENT_URI + "/" +
+                imageAssetFilePath);
+    }
+
+    /**
+     * Constructs a new {@link Attraction}.
+     *
+     * @param titleString is the title
+     * @param subtitleString is the short description
+     * @param descriptionString is the description
+     * @param addressString is the address
+     * @param location is the location
+     * @param categoryString is the category
+     * @param interestString is the interest
+     * @param ratesDouble is the interest
+     * @param numRatesInt is the interest
+     * @param openingString is the interest
+     * @param imageAssetFilePath is the file path from the application's assets folder for
+     *                           the image associated with this attraction
+     */
+
+    public Attraction(String titleString, String subtitleString, String descriptionString,
+                      String addressString, Location location, String categoryString,
+                      String interestString, double ratesDouble, int numRatesInt, String openingString,
+                      String imageAssetFilePath) {
+        mTitle = titleString;
+        mSubtitle = subtitleString;
+        mDescription = descriptionString;
+        mAddress = addressString;
+        mLocation = location;
+        mCategory = categoryString;
+        mInterest = interestString;
+        mRate = ratesDouble;
+        mNumRates = numRatesInt;
+        mOpeningTimes = openingString;
         mImageUri = Uri.parse("content://" + AssetProvider.CONTENT_URI + "/" +
                 imageAssetFilePath);
     }
@@ -48,9 +104,26 @@ public class Attraction implements Parcelable {
     private Attraction(Parcel parcel) {
         mTitle = parcel.readString();
         mSubtitle = parcel.readString();
-        //mLocation = Location.CREATOR.createFromParcel(parcel);
+        mLocation = Location.CREATOR.createFromParcel(parcel);
         mImageUri = Uri.parse(parcel.readString());
     }
+
+    /* New Attraction(Parcel parcel)
+
+    private Attraction(Parcel parcel) {
+        mTitle = parcel.readString();
+        mSubtitle = parcel.readString();
+	mDescription = parcel.readString();
+        mAddress = parcel.readString();
+        mLocation = Location.CREATOR.createFromParcel(parcel);
+	mCategory = parcel.readString();
+	mInterest = parcel.readDouble();
+        mRate = ratesDouble();
+	mNumRates = parcel.readInt();
+        mOpeningTimes = ratesString();
+        mImageUri = Uri.parse(parcel.readString());
+    }
+    */
 
     public String getTitle() {
         return mTitle;
@@ -58,6 +131,34 @@ public class Attraction implements Parcelable {
 
     public String getSubtitle() {
         return mSubtitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getAddress() {
+        return mAddress;
+    }
+
+    public String getCategory() {
+        return mCategory;
+    }
+
+    public String getInterest() {
+        return mInterest;
+    }
+
+    public double getRate() {
+        return mRate;
+    }
+
+    public int getNumRates() {
+        return mNumRates;
+    }
+
+    public String getOpeningTimes() {
+        return mOpeningTimes;
     }
 
     public Uri getImageUri() {
@@ -73,9 +174,27 @@ public class Attraction implements Parcelable {
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(mTitle);
         parcel.writeString(mSubtitle);
-        //mLocation.writeToParcel(parcel, i);
+        mLocation.writeToParcel(parcel, i);
         parcel.writeString(mImageUri.toString());
     }
+
+    /*
+    //New writeToParcel according new constructor
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mTitle);
+        parcel.writeString(mSubtitle);
+        parcel.writeString(mDescription);
+        parcel.writeString(mAddress);
+        mLocation.writeToParcel(parcel, i);
+        parcel.writeString(mCategory);
+        parcel.writeString(mInterest);
+        parcel.writeDouble(mRate);
+        parcel.writeInt(mNumRates);
+        parcel.writeString(mOpeningTimes);
+        parcel.writeString(mImageUri.toString());
+    }
+    */
 
     public static final Creator<Attraction> CREATOR = new Creator<Attraction>() {
         @Override

c2cb3c37a2f3e4a77ccce1209ba8d227ff23916d
villot <davillota@gmail.com>
2014-04-09 14:09:10 +0200

    Translator alfa test 1. English to Turkish. Please, DO NOT make a high usage of it. WE HAVE LIMITED WORDS TO TRANSLATE.

--- app/src/main/java/lbs/erasmus/touristanbul/TranslatorActivity.java
+++ app/src/main/java/lbs/erasmus/touristanbul/TranslatorActivity.java
@@ -5,34 +5,114 @@ import android.os.Bundle;
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
 
+import java.util.Locale;
+
+import android.os.AsyncTask;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 /**
  * Created by HP on 03/04/2014.
  */
 
-public class TranslatorActivity extends Activity {
+public class TranslatorActivity extends Activity implements OnInitListener {
+
+    private TextToSpeech tts;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translator);
+
+        tts = new TextToSpeech(this, this);
+        ((Button) findViewById(R.id.speechButton)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                speakOut(((TextView) findViewById(R.id.outputText)).getText().toString());
+            }
+        });
+
+
+        ((Button) findViewById(R.id.translateButton)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                class bgStuff extends AsyncTask<Void, Void, Void>{
+
+                    String translatedText = "";
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // TODO Auto-generated method stub
+
+                        try {
+                            String text = ((EditText) findViewById(R.id.inputText)).getText().toString();
+                            translatedText = translate(text);
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                            translatedText = e.toString();
+                        }
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        // TODO Auto-generated method stub
+                        ((TextView) findViewById(R.id.outputText)).setText(translatedText);
+                        super.onPostExecute(result);
+                    }
+
+                }
+                new bgStuff().execute();
+            }
+        });
     }
 
-    public static void main(String[] args) throws Exception {
+    public String translate(String text) throws Exception{
+
         //CLIENT_ID_HERE
         Translate.setClientId("erasmus-lbs-spain");
         //CLIENT_SECRET_HERE
         Translate.setClientSecret("eVRrtV6YIJNNqVxdbyobEPZQJI+dUoXd9O6C56TU2Ok=");
 
-        // Translate an english string to turkish
-        String englishString = "Hello World!";
-        String turkishTranslation = Translate.execute(englishString, Language.TURKISH);
-
-        System.out.println("Original english phrase: " + englishString);
-        System.out.println("Translated turkish phrase: " + turkishTranslation);
-    /*
-    OUTPUT Example:
-    Original english phrase: Hello World!
-    Translated spanish phrase: dünya merhaba!
-    */
+        String translatedText = "";
+        translatedText = Translate.execute(text, Language.TURKISH);
+        return translatedText;
     }
+
+    @Override
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.ENGLISH);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+
+                //speakOut("Oh yeaaaaaaah");
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!");
+        }
+    }
+
+    private void speakOut(String text) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
 }

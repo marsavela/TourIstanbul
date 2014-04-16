7db22c833160a5e68efa679a1e03a6fe833a2964
villot <davillota@gmail.com>
2014-04-16 18:09:52 +0200

    Orange Icons

--- app/src/main/java/lbs/erasmus/touristanbul/TranslatorActivity.java
+++ app/src/main/java/lbs/erasmus/touristanbul/TranslatorActivity.java
@@ -13,9 +13,12 @@ import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -26,11 +29,32 @@ import android.widget.TextView;
 public class TranslatorActivity extends Activity implements OnInitListener {
 
     private TextToSpeech tts;
-    //private Spinner translatedFrom, translatedTo;
+    private Spinner translatedFrom, translatedTo;
+    private String selected, spinner_item;
+    int translatedFromPosition, translatedToPosition;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translator);
+
+        //LanguageFrom Spinner Adapter
+        translatedFrom = (Spinner) findViewById(R.id.languageFrom);
+        ArrayAdapter<CharSequence> adapterLanguageFrom = ArrayAdapter.createFromResource(getApplicationContext(), R.array.supported_languages,
+                R.layout.spinner_item);
+
+        adapterLanguageFrom.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        translatedFrom.setAdapter(adapterLanguageFrom);
+        Log.v("VERBOSE", "Paso: " + translatedFrom.getCount());
+
+        //LanguageTo Spinner Adapter
+        translatedTo = (Spinner) findViewById(R.id.languageTo);
+        ArrayAdapter<CharSequence> adapterLanguageTo = ArrayAdapter.createFromResource(getApplicationContext(), R.array.supported_languages,
+                R.layout.spinner_item);
+
+        adapterLanguageTo.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        translatedTo.setAdapter(adapterLanguageTo);
+        Log.v("VERBOSE", "Paso: " + translatedTo.getCount());
 
         tts = new TextToSpeech(this, this);
         ((ImageButton) findViewById(R.id.speechButton)).setOnClickListener(new OnClickListener() {
@@ -42,6 +66,28 @@ public class TranslatorActivity extends Activity implements OnInitListener {
             }
         });
 
+        translatedFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            String textFrom = "English";
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                selected = translatedFrom.getSelectedItem().toString();
+                if (!selected.equals("Country"))
+                    spinner_item = selected;
+                System.out.println(selected);
+
+                //setid();
+            }
+
+//            private void setid() {
+//                translatedFrom.setSelection(translatedFromPosition);
+//                t.setText(spinner_item);
+//            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
 
         ((Button) findViewById(R.id.translateButton)).setOnClickListener(new OnClickListener() {
 
